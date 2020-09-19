677e953baba57b7ca65263adfc1d24853456e9f9
Chris Stark <starkfree@gmail.com>
2020-09-19 01:48:22 +0000

    feat: modifes validators to allow for empty input

--- paramvalidation/paramvalidation.go
+++ paramvalidation/paramvalidation.go
@@ -11,38 +11,38 @@ import (
 
 // Validate that a parameter contains only the values
 // of 1 or zero, and return that as a boolean
-func BoolParam( pVal, pName string ) (bool, bool, error) {
+func BoolParam( pVal, pName string ) (bool, error) {
 	if len(pVal) > 0 {
 		bInt, err := strconv.ParseUint( pVal, 10, 64 )
 		if err == nil && (bInt == 1 || bInt == 0) {
-			return bInt != 0, true, nil
+			return bInt != 0, nil
 		} else {
-			return false, true, errors.New( pName + ": can be only a 1 or 0" )
+			return false, errors.New( pName + ": can be only a 1 or 0" )
 		}
 	}
 
-	return false, false, nil
+	return false, nil
 }
 
 // Validate that a parameter contains only a uint
 // value, and return it
-func Uint64Param( pVal, pName string, allowZero bool ) (uint64, bool, error) {
+func Uint64Param( pVal, pName string, allowZero bool, defaultVal uint64 ) (uint64, error) {
 	if len(pVal) > 0 {
 		uintVal, err := strconv.ParseUint( pVal, 10, 64 )
 		if err == nil && (uintVal >= 0) {
 			if allowZero {
-				return uintVal, true, nil
+				return uintVal, nil
 			} else {
 				if uintVal != 0 {
-					return uintVal, true, nil
+					return uintVal, nil
 				} else {
-					return 0, true, errors.New( pName + ": must be a positive integer value greater than or equal to 1" )
+					return 0, errors.New( pName + ": must be a positive integer value greater than or equal to 1" )
 				}
 			}
 		} else {
-			return 0, true, errors.New( pName + ": must be a positive integer value greater than or equal to 1" )
+			return 0, errors.New( pName + ": must be a positive integer value greater than or equal to 1" )
 		}
 	}
 
-	return 0, false, nil
+	return defaultVal, nil
 }
--- paramvalidation/paramvalidation_test.go
+++ paramvalidation/paramvalidation_test.go
@@ -18,25 +18,23 @@ func TestParamValidation_BoolParam( t *testing.T ) {
 		pVal  		string
 		pName		string
 		expected  	bool
-		ok		bool
 		isErrorNil	bool
 	} {
-		{"Empty pVal","","param",false,false,true},
-		{"Valid pVal of 0","0","param",false,true,true},
-		{"Valid pVal of 1","1","param",true,true,true},
-		{"InValid pVal of 2","2","param",false,true,false},
-		{"InValid pVal of -2","-2","param",false,true,false},
-		{"InValid pVal string","t","param",false,true,false},
-		{"InValid pVal spaces","               ","param",false,true,false},
-		{"InValid pVal non-ascii","网络","param",false,true,false},
-		{"Valid pVal with empty pName","1","",true,true,true},
+		{"Empty pVal","","param",false,true},
+		{"Valid pVal of 0","0","param",false,true},
+		{"Valid pVal of 1","1","param",true,true},
+		{"InValid pVal of 2","2","param",false,false},
+		{"InValid pVal of -2","-2","param",false,false},
+		{"InValid pVal string","t","param",false,false},
+		{"InValid pVal spaces","               ","param",false,false},
+		{"InValid pVal non-ascii","网络","param",false,false},
+		{"Valid pVal with empty pName","1","",true,true},
 	}
 
 	for _,test := range tests {
 		testutils.OutputTestNote( t, test.testDesc )
-		result,ok,err := paramvalidation.BoolParam( test.pVal, test.pName )
+		result,err := paramvalidation.BoolParam( test.pVal, test.pName )
 		assert.Equal( t, test.expected, result )
-		assert.Equal( t, test.ok, ok)
 		if test.isErrorNil {
 			assert.Nil(t, err)
 		} else {
@@ -53,27 +51,26 @@ func TestParamValidation_Uint64Param( t *testing.T ) {
 		pVal  		string
 		pName		string
 		allowZero	bool
+		defaultVal	uint64
 		expected  	uint64
-		ok			bool
 		isErrorNil	bool
 	} {
-		{"Empty pVal","","param",false,0,false,true},
-		{"Valid pVal of 0","0","param",true,0,true,true},
-		{"Valid pVal of 1","1","param",false,1,true,true},
-		{"InValid pVal of 2","2","param",false,2,true,true},
-		{"InValid pVal of -2","-2","param",false,0,true,false},
-		{"InValid pVal string","t","param",false,0,true,false},
-		{"InValid pVal spaces","               ","param",false,0,true,false},
-		{"InValid pVal non-ascii","网络","param",false,0,true,false},
-		{"Valid pVal with empty pName","1","",false,1,true,true},
-		{"Valid pVal of 0 but AllowZero False","0","param",false,0,true,false},
+		{"Empty pVal","","param",false,100,100,true},
+		{"Valid pVal of 0","0","param",true,100,0,true},
+		{"Valid pVal of 1","1","param",false,100,1,true},
+		{"InValid pVal of 2","2","param",false,100,2,true},
+		{"InValid pVal of -2","-2","param",false,100,0,false},
+		{"InValid pVal string","t","param",false,100,0,false},
+		{"InValid pVal spaces","               ","param",false,100,0,false},
+		{"InValid pVal non-ascii","网络","param",false,100,0,false},
+		{"Valid pVal with empty pName","1","",false,100,1,true},
+		{"Valid pVal of 0 but AllowZero False","0","param",false,100,0,false},
 	}
 
 	for _,test := range tests {
 		testutils.OutputTestNote( t, test.testDesc )
-		result,ok,err := paramvalidation.Uint64Param( test.pVal, test.pName, test.allowZero )
+		result,err := paramvalidation.Uint64Param( test.pVal, test.pName, test.allowZero, test.defaultVal )
 		assert.Equal( t, test.expected, result )
-		assert.Equal( t, test.ok, ok)
 		if test.isErrorNil {
 			assert.Nil(t, err)
 		} else {
